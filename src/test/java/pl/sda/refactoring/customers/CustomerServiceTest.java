7904a5fbf68b7945e2c9039f5bd3d21b790d8b73
Kamil Jasek <kamil.jasek@dbschenker.com>
2020-12-11 09:52:20 +0100

    stage-3

--- src/test/java/pl/sda/refactoring/customers/CustomerServiceTest.java
+++ src/test/java/pl/sda/refactoring/customers/CustomerServiceTest.java
@@ -1,14 +1,29 @@
 package pl.sda.refactoring.customers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.mockito.ArgumentCaptor;
+import org.mockito.BDDMockito;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import pl.sda.refactoring.util.EmailSender;
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -31,5 +46,35 @@ final class CustomerServiceTest {
 
         // then
         assertFalse(result);
+    }
+
+    @Test
+    void shouldRegisterPerson() {
+        // given
+        given(customerDao.emailExists(anyString())).willReturn(false);
+        given(customerDao.peselExists(anyString())).willReturn(false);
+        var customerCaptor = ArgumentCaptor.forClass(Customer.class);
+
+        // when
+        final var result = customerService
+            .registerPerson("kamil@com.pl", "Kamil", "Jasek", "03039403944", false);
+
+        // then
+        assertTrue(result);
+
+        // then
+        assertTrue(result);
+        verify(customerDao, times(1)).save(customerCaptor.capture());
+        final var capturedCustomer = customerCaptor.getValue();
+        assertNotNull(capturedCustomer.getId());
+        assertNotNull(capturedCustomer.getCtime());
+        assertEquals("kamil@com.pl", capturedCustomer.getEmail());
+        assertEquals("Kamil", capturedCustomer.getfName());
+        assertEquals("Jasek", capturedCustomer.getlName());
+        assertFalse(capturedCustomer.isVerf());
+        assertNull(capturedCustomer.getVerfTime());
+        assertNull(capturedCustomer.getVerifBy());
+        assertNull(capturedCustomer.getCompName());
+        assertNull(capturedCustomer.getCompVat());
     }
 }
